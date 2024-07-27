5157d5aa9825e52991b3f02b2f09a5b743e7f220
CristianG5 <cristianse.galvan@gmail.com>
2024-07-27 05:19:26 -0300

    Initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,19 @@ import React from 'react';
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import TasksList from './components/TasksList'
 import TasksForm from './components/TasksForm';
+import Navbar from './components/Navbar'
+import {Container} from '@mui/material'
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Navbar />
+    <Container>
       <Routes>
         <Route path='/' element={<TasksList />}/>
         <Route path='/task/new' element={<TasksForm />}/>
       </Routes>
+      </Container>
     </BrowserRouter>
   )
 }
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,7 +1,24 @@
+import { AppBar, Box, Button, Container, Typography, Toolbar } from "@mui/material"
+import { Link, useNavigate } from "react-router-dom"
+
 export default function Navbar() {
+  const navigate = useNavigate();
   return (
-    <div>
-      Navbars
-    </div>
+    <Box sx={{flexGrow : 1}}>
+      <AppBar position="static" color="transparent">
+        <Container>
+          <Toolbar>
+            <Typography variant= 'h6' sx={{flexGrow : 1}}>
+              <Link to="/" style={{textDecoration:'none', color: '#eee'}} >Pern Task</Link>    
+            </Typography>
+
+            <Button variant="contained" color="primary" onClick={()=>navigate("/task/new")}>
+              New Task
+            </Button>
+
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </Box>
   )
 }
