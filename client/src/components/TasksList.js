7dae7b10c4380d9a4577b8cbf00f2c32c35389eb
CristianG5 <cristianse.galvan@gmail.com>
2024-08-05 23:38:20 -0300

    Lista de tareas

--- client/src/components/TasksList.js
+++ client/src/components/TasksList.js
@@ -1,9 +1,70 @@
-import React from 'react';
+import {useEffect, useState} from 'react';
+import {Button, Card, CardContent, Typography} from '@mui/material'
 
 export default function TasksList() {
+
+  const [tasks, setTasks] = useState([])
+
+  const loadTasks = async () => {
+   const response = await fetch('http://localhost:4000/tasks')
+   const data = await response.json()
+   //console.log(data)
+   setTasks(data)
+  }
+
+  useEffect(() =>{
+    loadTasks()
+  }, [])
+
   return (
-    <div>
-      Task Lists
-    </div>
+    <>
+      <h1>Task List</h1>
+    {
+      tasks.map(task =>(
+        <Card 
+          key={task.id}
+          style={{
+            marginBottom:'.7rem',
+            backgroundColor: '#1e272e',
+          }}  
+        >
+          <CardContent
+            style={{
+              display:'flex',
+              justifyContent:'space-between',
+            }}
+          >
+            <div
+              style={{color:'white'}}
+            >
+            <Typography>{task.title}</Typography>
+            <Typography>{task.description}</Typography>
+            </div>
+
+            <div>
+            <Button
+              variant='contained'
+              color='inherit'
+              onClick={() => console.log('edit')}
+            >
+              Edit
+            </Button>
+
+            <Button
+              variant='contained'
+              color='warning'
+              onClick={() => console.log('delete')}
+              style={{
+                marginLeft:'.5rem'
+              }}
+            >
+              Delete
+            </Button>
+            </div>
+          </CardContent>
+        </Card>
+      ))
+    }
+    </>
   )
 }
